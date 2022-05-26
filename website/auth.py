14ee0c12534537907cce02706d0cd656b99bddcb
JeanSolo <jeanpksolo@gmail.com>
2022-05-26 06:31:33 -0400

    feat: add signup functionality

--- website/auth.py
+++ website/auth.py
@@ -31,6 +31,37 @@ def login():
 def logout():
     pass
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    pass
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists!', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(first_name) < 2:
+            flash('First Name must be greater than 1 character', category='error')
+        elif len(last_name) < 2:
+            flash('Last Name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            login_user(user, remember=True)
+            # blueprint (router) + function name
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
