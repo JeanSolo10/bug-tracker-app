7b992e05af57b1597a3a23cf1803302c6bc4d007
JeanSolo <jeanpksolo@gmail.com>
2022-05-25 21:30:12 -0400

    feat: add login validation

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "bugtracker_db"
@@ -25,6 +26,15 @@ def create_app():
     from .models import User
 
     create_database(app)
+
+    # check for authenticated users and redirect accordingly
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
