15f2ad57aa00f6e292e603f8f74ca645298bd3d7
JeanSolo <jeanpksolo@gmail.com>
2022-05-25 21:17:50 -0400

    feat: add flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
