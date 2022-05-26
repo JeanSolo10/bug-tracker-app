38bfce7978c8e50a7e8b4141a426044f44de46b1
JeanSolo <jeanpksolo@gmail.com>
2022-05-25 21:13:53 -0400

    feat: add database

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv, find_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+db = SQLAlchemy()
+DB_NAME = "bugtracker_db"
 
 def create_app():
     app = Flask(__name__)
     load_dotenv(find_dotenv())
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # db config
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -14,4 +22,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    # check if database exists
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
